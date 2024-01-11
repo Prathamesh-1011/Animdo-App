265db424d501e763a93412e9fea252d3df75576e
Prathamesh Rokade <prathameshr955@gmail.com>
2024-01-11 21:49:05 +0530

    Basic Animations in flutter

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  double _buttonRadius = 100;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            _pageBackground(),
+            _circularAnimationButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _pageBackground() {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+
+  Widget _circularAnimationButton() {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _buttonRadius += _buttonRadius == 200 ? -100 : 100;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 2),
+          curve: Curves.bounceInOut,
+          height: _buttonRadius,
+          width: _buttonRadius,
+          decoration: BoxDecoration(
+            color: Colors.pink,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: const Center(
+            child: Text(
+              "Basic!",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
